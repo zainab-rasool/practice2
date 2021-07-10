8345cdc2bb45d2afd3282d2d88ed8f217415298f
Zainab <Zainab@DESKTOP-16U3LJS>
2021-07-10 21:21:18 +0530

    first time

---
+++ Z22/src/pkg/ClassBB.java
@@ -0,0 +1,13 @@
+package pkg;
+
+public class ClassBB {
+	
+	public void method1() {
+		System.out.println("method 1 of project Z22");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+
+}
